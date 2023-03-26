a429940e24e43e98764e3884f16917c9d5ca6d12
Durgesh Singh <rdbace012@gmail.com>
2023-03-26 09:37:40 -0700

    This is Flutter project where this project were made up of Basic of Scaffold widgets.

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome Home'),
+      ),
+      body: Center(
+        child: Text('We have pressed the button $_count times.'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _count++;
+        }),
+        tooltip: 'Increment Counter',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      drawer: Drawer(
+        elevation: 20.0,
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Durgesh Singh"),
+              accountEmail: Text("durgesh.singhcs20@smslucknow.ac.in"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("D"),
+              ),
+            ),
+            ListTile(
+              title: new Text("Inbox"),
+              leading: new Icon(Icons.mail),
+            ),
+            Divider( height: 0.1,),
+            ListTile(
+              title: new Text("Primary"),
+              leading: new Icon(Icons.inbox),
+            ),
+            ListTile(
+              title: new Text("Social"),
+              leading: new Icon(Icons.people),
+            ),
+            ListTile(
+              title: new Text("Promotions"),
+              leading: new Icon(Icons.local_offer),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
